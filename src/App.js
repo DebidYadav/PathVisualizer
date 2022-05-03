4a8ed900a5912eaf9ffcc95f10f2ff29ab974dfc
iamdebid <david23n23@gmail.com>
2022-05-03 23:24:34 +0530

    writing initials

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import './App.css';
+import pathfindingvisualizer from './Pathfindingvisualizer/Pathfindingvisualizer';
+
+function App(){
+  return(
+      <div className='App'>
+        <pathfindingvisualizer></pathfindingvisualizer>
+      </div>
+  );
+}
+
+export default App;
